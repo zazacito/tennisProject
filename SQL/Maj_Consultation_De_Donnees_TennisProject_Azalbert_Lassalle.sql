e9bd14b3ae7144d79db6ba593eadb30fe7345d44
zazacito <victor.azalbert@yahoo.fr>
2020-09-08 10:59:17 +0200

    MAJ_Consultation
    Créa

---
+++ SQL/Maj_Consultation_De_Donnees_TennisProject_Azalbert_Lassalle.sql
@@ -0,0 +1,34 @@
+/* description textuelles des requêtes de mise à jour */
+
+ /*2 requêtes impliquant 1 table*/
+ UPDATE JOUEUR
+ SET JOUEUR.JOCLASS = 12
+ WHERE JOUEUR.JOID = 1 ;
+
+UPDATE MATCH
+SET MATCH.MADATE = TO_DATE('2020/09/08', 'yyyy/mm/dd')
+WHERE MATCH.MATOUR = ‘Huitièmes’
+AND MATCH.TOID = 1;
+
+ /* 2 requêtes impliquant 2 tables*/
+
+ UPDATE (SELECT MATCH.JOU_JOID,
+                MATCH.MAWINNER winner,
+                MATCH.MASCORE score
+           FROM MATCH,
+                JOUEUR
+         WHERE MATCH.JOU_JOID = JOUEUR.JOID
+         AND JOUEUR.JOID = 8)
+    SET score = '6/4 6/0 6/0',
+        winner = 'Joueur 1';
+
+UPDATE (SELECT ARBITRE.ARID,
+               ARBITRE.ARSURNAME surname,
+               MATCH.ARID
+          FROM MATCH,
+               ARBITRE
+        WHERE MATCH.ARID = ARBITRE.ARID
+        AND MATCH.MAID = 8)
+   SET surname = 'Kévin';
+
+ /* 2 requêtes impliquant plus de 2 tables)*/
